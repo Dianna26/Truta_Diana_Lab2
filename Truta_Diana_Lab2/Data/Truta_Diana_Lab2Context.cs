6eaba6cef96458baa6fe7ff548c9a4741fc376e1
diann <diann@DRAGOS-PC>
2023-10-28 17:44:01 +0300

    Fac un push

---
+++ Truta_Diana_Lab2/Data/Truta_Diana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Truta_Diana_Lab2.Models;
+
+namespace Truta_Diana_Lab2.Data
+{
+    public class Truta_Diana_Lab2Context : DbContext
+    {
+        public Truta_Diana_Lab2Context (DbContextOptions<Truta_Diana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Truta_Diana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Truta_Diana_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Truta_Diana_Lab2/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Truta_Diana_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+    }
+}
